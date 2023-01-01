3429250eb73ff4582b3f8163f67575728c9d06b6
Wallace Nascimento <wallacediogo10@hotmail.com>
2023-01-01 20:28:10 -0300

    feature: add fetch hook | add axios | add loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useFetch } from './hooks/useFetch';
 
-type repository = {
+type Repository = {
   full_name: string;
   description: string
 }
 
 function App() {
-  const [repo, setRepo] = useState<repository[]>([])
-
-  useEffect(() => {
-    fetch('https://api.github.com/users/srnascimento40/repos')
-      .then(response => response.json())
-      .then(data => {
-        setRepo(data)
-      })
-  }, [])
-
+  const { data: repositories, isFetching } = useFetch<Repository[]>('https://api.github.com/users/srnascimento40/repos')
 
   return (
     <div className="App">
       <h1>basic form of fetching</h1>
       <ul>
-        {repo.map(repo => {
+        {isFetching && <p>Carregando...</p>}
+        {repositories?.map(repo => {
           return (
             <li key={repo.full_name}>
               <strong>{repo.full_name.split('SrNascimento40/')}</strong>
